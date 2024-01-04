672d867f78efb105e5e7ff90b3f7432c3195c712
lordanayami <matrixwenta92@interia.pl>
2024-01-04 17:04:48 +0100

    7.TODO

---
+++ FreeCodeCamp JavaScript Algorithms and Data Structures (Beta)/7.Todo App/dist/script.dev.js
@@ -0,0 +1,107 @@
+"use strict";
+
+var taskForm = document.getElementById("task-form");
+var confirmCloseDialog = document.getElementById("confirm-close-dialog");
+var openTaskFormBtn = document.getElementById("open-task-form-btn");
+var closeTaskFormBtn = document.getElementById("close-task-form-btn");
+var addOrUpdateTaskBtn = document.getElementById("add-or-update-task-btn");
+var cancelBtn = document.getElementById("cancel-btn");
+var discardBtn = document.getElementById("discard-btn");
+var tasksContainer = document.getElementById("tasks-container");
+var titleInput = document.getElementById("title-input");
+var dateInput = document.getElementById("date-input");
+var descriptionInput = document.getElementById("description-input");
+var taskData = JSON.parse(localStorage.getItem("data")) || [];
+var currentTask = {};
+
+var addOrUpdateTask = function addOrUpdateTask() {
+  var dataArrIndex = taskData.findIndex(function (item) {
+    return item.id === currentTask.id;
+  });
+  var taskObj = {
+    id: "".concat(titleInput.value.toLowerCase().split(" ").join("-"), "-").concat(Date.now()),
+    title: titleInput.value,
+    date: dateInput.value,
+    description: descriptionInput.value
+  };
+
+  if (dataArrIndex === -1) {
+    taskData.unshift(taskObj);
+  } else {
+    taskData[dataArrIndex] = taskObj;
+  }
+
+  localStorage.setItem("data", JSON.stringify(taskData));
+  updateTaskContainer();
+  reset();
+};
+
+var updateTaskContainer = function updateTaskContainer() {
+  tasksContainer.innerHTML = "";
+  taskData.forEach(function (_ref) {
+    var id = _ref.id,
+        title = _ref.title,
+        date = _ref.date,
+        description = _ref.description;
+    return tasksContainer.innerHTML += "\n      <div class=\"task\" id=\"".concat(id, "\">\n        <p><strong>Title:</strong> ").concat(title, "</p>\n        <p><strong>Date:</strong> ").concat(date, "</p>\n        <p><strong>Description:</strong> ").concat(description, "</p>\n        <button onclick=\"editTask(this)\" type=\"button\" class=\"btn\">Edit</button>\n        <button onclick=\"deleteTask(this)\" type=\"button\" class=\"btn\">Delete</button> \n      </div>\n    ");
+  });
+};
+
+var deleteTask = function deleteTask(buttonEl) {
+  var dataArrIndex = taskData.findIndex(function (item) {
+    return item.id === buttonEl.parentElement.id;
+  });
+  buttonEl.parentElement.remove();
+  taskData.splice(dataArrIndex, 1);
+  localStorage.setItem("data", JSON.stringify(taskData));
+};
+
+var editTask = function editTask(buttonEl) {
+  var dataArrIndex = taskData.findIndex(function (item) {
+    return item.id === buttonEl.parentElement.id;
+  });
+  currentTask = taskData[dataArrIndex];
+  titleInput.value = currentTask.title;
+  dateInput.value = currentTask.date;
+  descriptionInput.value = currentTask.description;
+  addOrUpdateTaskBtn.innerText = "Update Task";
+  addOrUpdateTaskBtn.ariaLabel = "Update Task";
+  taskForm.classList.toggle("hidden");
+};
+
+var reset = function reset() {
+  titleInput.value = "";
+  dateInput.value = "";
+  descriptionInput.value = "";
+  taskForm.classList.toggle("hidden");
+  currentTask = {};
+};
+
+if (taskData.length) {
+  updateTaskContainer();
+}
+
+openTaskFormBtn.addEventListener("click", function () {
+  return taskForm.classList.toggle("hidden");
+});
+closeTaskFormBtn.addEventListener("click", function () {
+  var formInputsContainValues = titleInput.value || dateInput.value || descriptionInput.value;
+  var formInputValuesUpdated = titleInput.value !== currentTask.title || dateInput.value !== currentTask.date || descriptionInput.value !== currentTask.description;
+
+  if (formInputsContainValues && formInputValuesUpdated) {
+    confirmCloseDialog.showModal();
+  } else {
+    reset();
+  }
+});
+cancelBtn.addEventListener("click", function () {
+  return confirmCloseDialog.close();
+});
+discardBtn.addEventListener("click", function () {
+  confirmCloseDialog.close();
+  reset();
+});
+taskForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  addOrUpdateTask();
+});
