8be9278a14c1ae357ed1c893dbb110c98c100e86
lordanayami <matrixwenta92@interia.pl>
2024-04-19 15:30:06 +0200

    cssStyles

---
+++ Bro Code React Full Course for free ⚛️ (2024)/my-react-app3/src/App.jsx
@@ -0,0 +1,13 @@
+import Button from "./Button.jsx";
+//Styling: external in index.css, modules like Button.module.css + Button.jsx in folder or inline like in BUtton.jsx now
+
+function App() {
+  return(
+    <>
+    <Button />
+    </>
+    
+  );
+}
+
+export default App
