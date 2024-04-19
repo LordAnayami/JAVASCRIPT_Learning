d96174aed37552ea80ed2e1a2141e08044e43850
lordanayami <matrixwenta92@interia.pl>
2024-04-19 16:03:06 +0200

    props

---
+++ Bro Code React Full Course for free ⚛️ (2024)/my-react-app4/src/App.jsx
@@ -0,0 +1,17 @@
+import Student from "./Student.jsx";
+//Props
+
+function App() {
+  return(
+    <>
+    <Student name="Spongebob" age={30} isStudent={true} />
+    <Student name="Patric" age={42} isStudent={false} />
+    <Student name="Squidward" age={50} isStudent={false} />
+    <Student name="Sandy" age={27} isStudent={true} />
+    <Student />
+    </>
+    
+  );
+}
+
+export default App
