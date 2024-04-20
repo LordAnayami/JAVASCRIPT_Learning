d6a9d6fd0d4e66bb4ece0cfdd9ce6ecf73ae9012
lordanayami <matrixwenta92@interia.pl>
2024-04-20 15:02:26 +0200

    useState hook

---
+++ Bro Code React Full Course for free ⚛️ (2024)/my-react-app8/src/App.jsx
@@ -0,0 +1,14 @@
+import MyComponent from "./MyComponent.jsx"
+import Counter from "./Counter.jsx"
+//useState() hook
+
+function App() {
+  return (<>
+    <MyComponent />
+    <Counter />
+    </>
+  )
+  
+}
+
+export default App
