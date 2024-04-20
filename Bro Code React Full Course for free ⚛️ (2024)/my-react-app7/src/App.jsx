842d9eb424a989f0a0c992c5253d18836d6c5e9e
lordanayami <matrixwenta92@interia.pl>
2024-04-20 14:00:08 +0200

    click events

---
+++ Bro Code React Full Course for free ⚛️ (2024)/my-react-app7/src/App.jsx
@@ -0,0 +1,14 @@
+import Button from "../../my-react-app7/src/Button.jsx";
+import ProfilePicture from "./ProfilePicture.jsx";
+//click event
+
+function App() {
+  return (<>
+    <Button />
+    <ProfilePicture />
+    </>
+  )
+  
+}
+
+export default App
