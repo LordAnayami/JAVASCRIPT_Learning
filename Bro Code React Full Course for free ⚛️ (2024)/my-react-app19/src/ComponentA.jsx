79509ee062277912a8cafad45db8bd3b75d445b7
lordanayami <matrixwenta92@interia.pl>
2024-04-21 19:17:24 +0200

    useRef

---
+++ Bro Code React Full Course for free ⚛️ (2024)/my-react-app19/src/ComponentA.jsx
@@ -0,0 +1,54 @@
+import React, {useState, useEffect, useRef} from "react";
+
+function ComponentA(){
+    //useReff do not rerender all document
+    //let [number, setNumber] = useState(0);
+    const inputRef1 = useRef(null);
+    const inputRef2 = useRef(null);
+    const inputRef3 = useRef(null);
+
+    useEffect(() => {
+        console.log("CR");
+       
+    });
+
+    function handleClick1(){
+       inputRef1.current.focus();
+       inputRef1.current.style.backgroundColor = "yellow";
+       inputRef2.current.style.backgroundColor = "";
+       inputRef3.current.style.backgroundColor = "";
+    }
+
+    function handleClick2(){
+        inputRef2.current.focus();
+        inputRef1.current.style.backgroundColor = "";
+        inputRef2.current.style.backgroundColor = "yellow";
+        inputRef3.current.style.backgroundColor = "";
+     }
+
+     function handleClick3(){
+        inputRef3.current.focus();
+        inputRef1.current.style.backgroundColor = "";
+        inputRef2.current.style.backgroundColor = "";
+        inputRef3.current.style.backgroundColor = "yellow";
+     }
+
+    return(
+        <>
+        <button onClick={handleClick1}>
+            Click me 1!
+        </button>
+        <input ref={inputRef1}/><br/>
+        <button onClick={handleClick2}>
+            Click me 2!
+        </button>
+        <input ref={inputRef2}/><br/>
+        <button onClick={handleClick3}>
+            Click me 3!
+        </button>
+        <input ref={inputRef3}/><br/>
+        </>
+        
+    )
+}
+export default ComponentA
