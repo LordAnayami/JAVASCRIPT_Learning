32fab39ab8bc5c9d0d07abb1cb441321c577779f
Mateusz.Wenta <matrixwenta92@interia.pl>
2024-12-28 01:37:24 +0100

    poprawny commit

--- Udemy Next.js 15 and React - The Complete Guide/next-app-1-food-app/food-app/app/not-found.js
+++
@@ -1,6 +0,0 @@
-export default function NotFound(){
-    return <main className="not-found">
-        <h1>Not found</h1>
-        <p>Unfortunetely, we could not find the requested page or resource.</p>
-    </main>
-}
