78afed2e8c9b42f7bf97ea4410d921cdc9a6be8b
Mateusz.Wenta <matrixwenta92@interia.pl>
2025-01-02 18:38:44 +0100

    small change

--- Udemy Next.js 15 and React - The Complete Guide/16-auth/lib/db.js
+++ Udemy Next.js 15 and React - The Complete Guide/16-auth/lib/db.js
@@ -2,7 +2,7 @@ import { MongoClient } from 'mongodb';
 
 export async function connectToDatabase() {
   const client = await MongoClient.connect(
-    'mongodb+srv://lordanayami:A12345678b!@cluster0.bg1k4.mongodb.net/auth-demo?retryWrites=true&w=majority'
+    'mongodb+srv://lordanayami:password@cluster0.bg1k4.mongodb.net/auth-demo?retryWrites=true&w=majority'
   );
 
   return client;
