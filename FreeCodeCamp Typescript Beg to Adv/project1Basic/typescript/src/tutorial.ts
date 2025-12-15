6463ad6be77d1511ae779115a5021c1361907b58
Mateusz.Wenta <matrixwenta92@interia.pl>
2025-12-15 18:29:02 +0100

    tasks list

---
+++ FreeCodeCamp Typescript Beg to Adv/project1Basic/typescript/src/tutorial.ts
@@ -0,0 +1,23 @@
+type Employee = { id: number; name: string; department: string };
+type Manager = { id: number; name: string; employees: Employee[] };
+
+type Staff = Employee | Manager;
+
+function printStaffDetails(staff: Staff) {
+  if ("employees" in staff) {
+    console.log(
+      `${staff.name} is a manager of ${staff.employees.length} employees.`
+    );
+  } else {
+    console.log(
+      `${staff.name} is an employee in the ${staff.department} department.`
+    );
+  }
+}
+
+const alice: Employee = { id: 1, name: "Alice", department: "Sales" };
+const steve: Employee = { id: 1, name: "Steve", department: "HR" };
+const bob: Manager = { id: 2, name: "Bob", employees: [alice, steve] };
+
+printStaffDetails(alice); // Outputs: Alice is an employee in the Sales department.
+printStaffDetails(bob);
