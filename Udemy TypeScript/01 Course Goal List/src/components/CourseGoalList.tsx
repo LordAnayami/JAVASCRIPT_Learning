8ffc8b9611e1a41570bec3f08cf71bdf83e0c22f
Mateusz.Wenta <matrixwenta92@interia.pl>
2026-01-04 18:16:00 +0100

    1st project end

---
+++ Udemy TypeScript/01 Course Goal List/src/components/CourseGoalList.tsx
@@ -0,0 +1,88 @@
+import { type ReactNode } from "react";
+
+import CourseGoal from "./CourseGoal.tsx";
+import { type CourseGoal as CGoal } from "../App.tsx";
+import InfoBox from "./InfoBox.tsx";
+
+type CourseGoalListProps = {
+  goals: CGoal[];
+  onDeleteGoal: (id: number) => void;
+};
+
+export default function CourseGoalList({
+  goals,
+  onDeleteGoal,
+}: CourseGoalListProps) {
+  if (goals.length === 0) {
+    return (
+      <InfoBox mode="hint">
+        You have no course goals yet. Start adding some!
+      </InfoBox>
+    );
+  }
+
+  let warningBox: ReactNode;
+
+  if (goals.length >= 4 && goals.length < 7) {
+    warningBox = (
+      <InfoBox mode="warning" severity="low">
+        You're collecting a lot of goals. Don't put too much on your plate!
+      </InfoBox>
+    );
+  } else if (goals.length >= 7 && goals.length < 10) {
+    warningBox = (
+      <InfoBox mode="warning" severity="medium">
+        You're collecting a lot of goals. Don't put too much on your plate!
+      </InfoBox>
+    );
+  } else if (goals.length >= 10) {
+    warningBox = (
+      <InfoBox mode="warning" severity="high">
+        You're collecting a lot of goals. Don't put too much on your plate!
+      </InfoBox>
+    );
+  }
+
+  return (
+    <>
+      {warningBox}
+      <ul>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            <CourseGoal id={goal.id} title={goal.title} onDelete={onDeleteGoal}>
+              <p>{goal.description}</p>
+            </CourseGoal>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+//My solution:
+// import CourseGoal from "./CourseGoal.tsx";
+// import { type CourseGoal as CGoal } from "../App.tsx";
+
+// type CourseGoalListProps = {
+//   goals: CGoal[];
+//   onDeleteGoal: (id: number) => void;
+// };
+
+// export default function CourseGoalList({
+//   goals,
+//   onDeleteGoal,
+// }: CourseGoalListProps) {
+//   return (
+//     <div>
+//       <ul>
+//         {goals.map((goal) => (
+//           <li key={goal.id}>
+//             <CourseGoal title={goal.title} id={goal.id} onDelete={onDeleteGoal}>
+//               <p>{goal.description}</p>
+//             </CourseGoal>
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
